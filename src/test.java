3c01f0941607c385bea342eed590f0f3a180a17e
AhnJaeHyeon <ajh9080@naver.com>
2016-10-05 11:46:36 +0900

    www

--- src/test.java
+++ src/test.java
@@ -67,6 +67,8 @@ public class test {
 		
 		String gitTest = "master ºê·»Ä¡ Ä¿¹Ô";
 		String gitTest = "master ºê·»Ä¡ µÎ¹øÂ° Ä¿¹Ô"; 
+		String gitTest = "master ºê·»Ä¡ µÎ¹øÂ° Ä¿¹Ô"; 
+		String gitTest = "master ºê·»Ä¡ µÎ¹øÂ° Ä¿¹Ô"; 
 		
 		System.out.println(mnt_Week);
 	}
